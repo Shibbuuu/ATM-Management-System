a490daf1f12278da8ac4a91de7ace98feaf9a140
Shibbuuu <91153318+Shibbuuu@users.noreply.github.com>
2023-04-07 19:57:46 +0530

    Add files via upload

---
+++ ATM_machine/src/atm_machine/PreparedStatement.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package atm_machine;
+
+/**
+ *
+ * @author Shiv Singh
+ */
+class PreparedStatement {
+    
+}
